594729bc1ca1d4716a23a8307fd62108fe7d8c1a
ConorMB93 <Conor.dit93@gmail.com>
2018-09-22 23:16:23 +0100

    Changes for player and ground.
    Player and ground. Player moves forward at constand speed.

---
+++ Switch/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D playerRigidBody;
+    public float playerSpeed;
+    public float playerJumpHieght;
+
+	// Use this for initialization
+	void Start () {
+        
+        playerRigidBody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        playerRigidBody.velocity = new Vector2(playerSpeed, playerRigidBody.position.y);
+
+	}
+}
