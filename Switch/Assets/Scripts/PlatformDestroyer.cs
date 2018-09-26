dde205ed4c02aef3d757361cc5a0a26266dd3fc1
ConorMB93 <Conor.dit93@gmail.com>
2018-09-26 21:45:51 +0100

    Platform Generator Start
    Scripts for generating platforms. Object pooler model in progress.

---
+++ Switch/Assets/Scripts/PlatformDestroyer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDestroyer : MonoBehaviour {
+
+    public GameObject platformDestructionPoint;
+
+	// Use this for initialization
+	void Start () {
+
+        platformDestructionPoint = GameObject.Find("PlatformDestructionPoint");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        DestroyPlatform();
+       
+	}
+
+    private void DestroyPlatform(){
+        
+        if (transform.position.x < platformDestructionPoint.transform.position.x){
+            gameObject.SetActive(false);
+
+        }
+    }
+}
---
+++ Switch/Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    
+
+    public GameObject platformGenerationPoint;
+    public GameObject platform;
+
+    private float platformWidth;
+
+	// Use this for initialization
+	void Start () {
+
+        platformGenerationPoint = GameObject.Find("PlatformGenerationPoint");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+    private void GeneratePlatforms(){
+
+        if (transform.position.x < platformGenerationPoint.transform.position.x){
+
+            transform.position = new Vector3(transform.position.x + GetPlatformWidth(), transform.position.y);
+        }
+    }
+
+    private float GetPlatformWidth(){
+
+        platformWidth = platform.GetComponent<BoxCollider2D>().size.x;
+
+        return platformWidth;
+    } 
+   
+}
